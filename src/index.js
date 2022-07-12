3931acadf98cb2bbd8a5202f991b732e74ce7b9e
taha-fituri <106868510+taha-fituri@users.noreply.github.com>
2022-07-12 11:20:59 +0100

    Update index.js
    delete warning

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "antd/dist/antd.css";
+import 'antd/dist/antd.min.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
